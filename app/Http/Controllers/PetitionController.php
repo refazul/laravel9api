2d34d494282e1a46dcada2b951468cbb84a12e63
Refazul <refazul.refat@yahoo.com>
2022-09-19 19:21:59 +0600

    {{petitions}} CRUD API

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -2,41 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PetitionCollection;
+use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class PetitionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return PetitionCollection
      */
     public function index()
     {
-        //
+        return new PetitionCollection(Petition::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function store(Request $request)
     {
-        //
+        $petition = Petition::create($request->only([
+            'title', 'description', 'category', 'author', 'signees'
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function show(Petition $petition)
     {
-        //
+        return response()->json(data: new PetitionResource($petition), status: Response::HTTP_OK);
     }
 
     /**
@@ -44,11 +53,15 @@ class PetitionController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function update(Request $request, Petition $petition)
     {
-        //
+        $petition->update($request->only([
+            'title', 'description', 'category', 'author', 'signees'
+        ]));
+
+        return response()->json(data: new PetitionResource($petition), status: Response::HTTP_OK);
     }
 
     /**
@@ -59,6 +72,8 @@ class PetitionController extends Controller
      */
     public function destroy(Petition $petition)
     {
-        //
+        $petition->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
